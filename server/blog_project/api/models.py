f91e8a41a301ad445679f4dbd834fe6b6462a92f
Shreya Kumari <shreyarajput600@gmail.com>
2024-12-11 16:18:17 +0530

    done

--- server/blog_project/api/models.py
+++ server/blog_project/api/models.py
@@ -8,17 +8,6 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-# class Blog(models.Model):
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
-#     author = models.ForeignKey(
-#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs'
-#     )
-#     date_created = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.title
 
 class Blog(models.Model):
     CATEGORY_CHOICES = [
